4f921dbd8de9f8db300c521767accd3140fd6f4a
Arkadiusz Zygan <a.zygan@gmail.com>
2021-09-23 14:25:46 +0200

    Style item component

--- src/components/Item.styles.ts
+++ src/components/Item.styles.ts
@@ -1,3 +1,28 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  border: 1px solid lightblue;
+  border-radius: 20px;
+
+  button {
+    border-radius: 0 0 20px 20px;
+    background-color: lightblue;
+  }
+
+  img {
+    max-height: 250px;
+    object-fit: cover;
+    border-radius: 20px 20px 0 0;
+  }
+
+  div {
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 16px;
+    height: 100%;
+  }
+`;
