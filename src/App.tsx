eaa2cdd54b5e9502cde0b6f43a8e15ac7cf1596a
Arkadiusz Zygan <a.zygan@gmail.com>
2021-09-23 14:04:43 +0200

    Fetch data from fakeapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { useQuery } from "react-query";
+// styles
+import { Wrapper } from "./App.styles";
+
+export type CartTypeItem = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartTypeItem[]> =>
+  (await fetch("https://fakestoreapi.com/products")).json();
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartTypeItem[]>(
+    "products",
+    getProducts
+  );
+  console.log(data);
   return (
-    <div>
+    <Wrapper>
       <h1>Start project</h1>
-    </div>
+    </Wrapper>
   );
 };
 
